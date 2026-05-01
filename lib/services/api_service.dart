7520b45cfb3e8384ccc6537fa4b2978dfdfa0253
alphadude <alphadude4all@gmail.com>
2026-05-01 08:21:22 +0100

    Fixed API response parsing to handle wrapped 'data' object in backend response

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,7 +9,8 @@ class ApiService {
     try {
       final response = await http.get(Uri.parse('$baseUrl/vessels'));
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
+        final Map<String, dynamic> responseData = json.decode(response.body);
+        final List<dynamic> data = responseData['data'] ?? [];
         return data.map((json) => Vessel.fromJson(json)).toList();
       } else {
         throw Exception('Failed to load vessels');
@@ -23,7 +24,8 @@ class ApiService {
     try {
       final response = await http.get(Uri.parse('$baseUrl/equipment'));
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
+        final Map<String, dynamic> responseData = json.decode(response.body);
+        final List<dynamic> data = responseData['data'] ?? [];
         return data.map((json) => Equipment.fromJson(json)).toList();
       } else {
         throw Exception('Failed to load equipment');
